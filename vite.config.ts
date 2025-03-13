505db792b50f621617c28204d97bc394debf0120
LuisJimenez19 <luisjian948@gmail.com>
2025-03-13 15:24:18 -0300

    fix : change base for repositorie name

--- vite.config.ts
+++ vite.config.ts
@@ -10,5 +10,5 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: "https://github.com/LuisJimenez19/ticket-generator/",
+  base: "/ticket-generator/",
 });
